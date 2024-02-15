26fbffce13945028c2a5d9c9a516b231a7335acc
luteor <leo.turco.83@gmail.com>
2024-02-15 16:48:34 +0100

    build: :art: Set up and configure a path system with aliases for imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -10,7 +10,22 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      alias: {
+        extensions: [".js", ".jsx"],
+        map: [
+          ["@assets", "./src/assets"],
+          ["@components", "./src/components"],
+          ["@contexts", "./src/contexts"],
+          ["@hooks", "./src/hooks"],
+          ["@utils", "./src/utils"],
+          ["public", "./public"],
+        ],
+      },
+    },
+  },
   plugins: ["react-refresh"],
   rules: {
     "react/jsx-no-target-blank": "off",
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@assets": `${path.resolve(__dirname, "src/assets")}`,
+      "@components": `${path.resolve(__dirname, "src/components")}`,
+      "@contexts": `${path.resolve(__dirname, "src/contexts")}`,
+      "@hooks": `${path.resolve(__dirname, "src/hooks")}`,
+      "@utils": `${path.resolve(__dirname, "src/utils")}`,
+      "@public": `${path.resolve(__dirname, "public")}`,
+    },
+  },
+});
