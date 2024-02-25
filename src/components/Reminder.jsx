82ef32e0df2e1294444ad3d94a9f7b59cf325ea1
luteor <leo.turco.83@gmail.com>
2024-02-25 17:54:51 +0100

    feat: :sparkles: Flag icon added to flagged reminders

--- src/components/Reminder.jsx
+++ src/components/Reminder.jsx
@@ -1,3 +1,5 @@
+import { IoIosFlag } from "react-icons/io";
+
 import { getDisplayColors } from "@utils/getDisplayColors";
 
 export const Reminder = ({
@@ -30,20 +32,23 @@ export const Reminder = ({
   const { checkedBgColor, checkedOutlineColor } = getDisplayColors(listColor);
 
   return (
-    <div className="flex flex-row items-center justify-start gap-2 border-b border-solid pb-2 pl-2">
-      <input
-        type="checkbox"
-        name="reminder"
-        id="reminder"
-        className={`peer flex h-4 w-4 appearance-none items-center justify-center rounded-full  ${
-          isCompletedRemindersDisplayed
-            ? "border-none bg-gray-500 outline outline-1 outline-offset-1 outline-gray-500 checked:border checked:border-solid checked:border-gray-400 checked:bg-white checked:outline-none"
-            : `bg-white ${checkedBgColor} ${checkedOutlineColor} border border-solid border-gray-400 checked:border-none checked:outline checked:outline-1 checked:outline-offset-1`
-        }`}
-        onChange={handleCheckReminder}
-      />
+    <div className="flex flex-row items-center justify-between border-b border-solid p-2">
+      <div className="flex flex-row items-center justify-start gap-2">
+        <input
+          type="checkbox"
+          name="reminder"
+          id="reminder"
+          className={`peer flex h-4 w-4 appearance-none items-center justify-center rounded-full  ${
+            isCompletedRemindersDisplayed
+              ? "border-none bg-gray-500 outline outline-1 outline-offset-1 outline-gray-500 checked:border checked:border-solid checked:border-gray-400 checked:bg-white checked:outline-none"
+              : `bg-white ${checkedBgColor} ${checkedOutlineColor} border border-solid border-gray-400 checked:border-none checked:outline checked:outline-1 checked:outline-offset-1`
+          }`}
+          onChange={handleCheckReminder}
+        />
 
-      <label htmlFor="reminder">{reminder.content}</label>
+        <label htmlFor="reminder">{reminder.content}</label>
+      </div>
+      {reminder.flag ? <IoIosFlag className="h-3 w-3 text-orange-500" /> : null}
     </div>
   );
 };
